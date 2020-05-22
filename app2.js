7590bb29328bb888376b08183712275a9b73d36a
Thomas Wheeler <tbwheeler94@gmail.com>
2020-05-21 21:02:27 -0700

    Third Push

--- app2.js
+++ app2.js
@@ -1,15 +1,27 @@
+//Select test subject ID no. and assign to a dropdown
 var idSelect = d3.select("#selDataset");
 
+//Select json data file samples
 d3.json("samples.json").then((data) => {
+    
+    //Navivate to the names array
     var data_name = data.names;
+
+        //Iterate through each value in the array
         data_name.forEach((name, index) => {
+
+            // Append option html tags to #selDataset
             var name_select = idSelect.append("option");
+
+            // Add text to dropdown tags
             name_select.text(name);
+
+            // Data-bind index value
             name_select.attr("value",`${index}`); 
         });
         });
 
-//Initialize bar graph
+//Create function for Bar Chart
 
 function initBar() {
     d3.json("samples.json").then((data) => {
@@ -21,13 +33,11 @@ function initBar() {
         var grabx = sample_object[2]
         var slicex = grabx.slice(0, 10);
         var x_values = slicex.reverse()
-        
 
         //Grab otu values
         var graby = sample_object[1]
         var slicey = graby.slice(0, 10);
         var y_values = slicey.reverse()
-       
 
         //Map OTU items to string
         var y_axis = y_values.map(item => `OTU ${item}`)
@@ -55,6 +65,8 @@ function initBar() {
 };
 
 initBar();
+
+// Creation function for Bubble chart
 
 function initBubble() {
     d3.json("samples.json").then((data) => {
@@ -103,21 +115,19 @@ function initBubble() {
 
 initBubble();
 
+// Create default display in Demographic Info Panel
 
 function initPanel() {
     d3.json("samples.json").then((data) => {
         
-        //Grab metadata object
-        
+        //Grab keys and values from first object in the array
         var sample_keys = Object.keys(data.metadata[0]);
         var sample_values = Object.values(data.metadata[0]);
 
-
+        //Select demographic info panel
         var addList = d3.select("#sample-metadata");
 
-        //  
-        addList.html("");
-
+        //Loop through 7 values, append keys and values to an unordered list
         for (var i = 0; i < 7; i++) {
             var name_select = addList.append("ul");
             name_select.text(`${sample_keys[i]}: ${sample_values[i]}`);
@@ -128,6 +138,8 @@ function initPanel() {
 };
 
 initPanel();
+
+// Create default display panel which updates when new value is selected
 
 idSelect.on("change", optionChanged);
 
@@ -145,8 +157,10 @@ idSelect.on("change", optionChanged);
 
   var addList = d3.select("#sample-metadata");
 
+  //Clear existing values in panel
   addList.html("");
 
+        //Loop through 7 items in arrays, append keys and values to an unordered list
         for (var i = 0; i < 7; i++) {
             var name_select = addList.append("ul");
             name_select.text(`${filtered_key[i]}: ${filtered_value[i]}`);
